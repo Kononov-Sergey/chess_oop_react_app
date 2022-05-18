7b3346895702ca4cfe16b3325b82b8037802cce4
Sergei Kononov <kobo220376@gmail.com>
2022-05-18 16:54:02 +0300

    added smooth animation for avaliable move and made a moveset for the pawn figure

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -6,6 +6,8 @@ import blackPawnLogo from "../../assets/black-pawn.png";
 import whitePawnLogo from "../../assets/white-pawn.png";
 
 export class Pawn extends Figure {
+  isFirstMove: boolean = true;
+
   constructor(cell: CellClass, color: Colors) {
     super(cell, color);
     this.logo = this.color === Colors.BLACK ? blackPawnLogo : whitePawnLogo;
@@ -16,6 +18,32 @@ export class Pawn extends Figure {
     if (!super.canMove(target)) {
       return false;
     }
-    return true;
+
+    const direction = this.cell.figure?.color === Colors.BLACK ? -1 : 1;
+    const firstStepMoveDirection =
+      this.cell.figure?.color === Colors.BLACK ? -2 : 2;
+
+    if (
+      target.y - this.cell.y === direction &&
+      (target.x === this.cell.x + 1 || target.x === this.cell.x - 1) &&
+      target.figure !== null
+    ) {
+      return true;
+    }
+
+    if (
+      (target.y === this.cell.y + direction || // first cell against our figure
+        (this.isFirstMove &&
+          target.y === this.cell.y + firstStepMoveDirection)) && // checking, whether is a first move
+      this.cell.board.getCell(target.x, target.y).withoutAFigure() && // checking, is cell has a figure within
+      target.x === this.cell.x
+    ) {
+      return true;
+    }
+    return false;
+  }
+  moveFigure(target: CellClass): void {
+    super.moveFigure(target);
+    this.isFirstMove = false;
   }
 }
